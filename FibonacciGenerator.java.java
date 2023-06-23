180559cb0e9d051530a8c83b436dfb28b8497f3a
Koechiii <koechkevin24@gmail.com>
2023-06-23 09:34:48 +0300

    Added comments explaining the code logics Closed the scanner

--- FibonacciGenerator.java.java
+++ FibonacciGenerator.java.java
@@ -10,6 +10,7 @@ class FibonacciGenerator {
         int numTerms;
         int firstTerm;
 
+        // Prompt the user to enter the number of terms in the Fibonacci sequence
         do {
             System.out.print("Enter the number of terms in the Fibonacci sequence (first term must be 0 or 1): ");
 
@@ -17,11 +18,13 @@ class FibonacciGenerator {
             numTerms = Integer.parseInt(input);
             firstTerm = numTerms / (int) Math.pow(10, input.length() - 1);
 
+            // Validate the first term
             if (firstTerm != 0 && firstTerm != 1) {
                 System.out.println("Invalid input. The first term must be either 0 or 1.");
                 continue;
             }
 
+            // Validate the number of terms
             if (numTerms <= 0) {
                 System.out.println("Invalid input. Please enter a positive integer for the number of terms.");
                 continue;
@@ -39,15 +42,20 @@ class FibonacciGenerator {
             fibCache[i] = -1;
         }
 
+        // Generate and print the Fibonacci sequence
         generateFibonacciSequence(numTerms, firstTerm);
+
+        scanner.close(); // Close the Scanner object
     }
 
+    // Generate and print the Fibonacci sequence
     public static void generateFibonacciSequence(int numTerms, int firstTerm) {
         for (int i = 0; i < numTerms; i++) {
             System.out.print(fibonacci(i, firstTerm) + ", ");
         }
     }
 
+    // Recursive method to compute the Fibonacci number at index 'n'
     public static long fibonacci(int n, int firstTerm) {
         // Check if the Fibonacci number is already computed and cached
         if (fibCache[n] != -1) {
